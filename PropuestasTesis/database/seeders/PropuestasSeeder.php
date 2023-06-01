8226ab4ed0cae71fed91d63ac61e74c601d2a8da
ValaaGon <vale.gon1218@gmail.com>
2023-05-31 19:13:22 -0600

    Seeders con carbon

--- PropuestasTesis/database/seeders/PropuestasSeeder.php
+++ PropuestasTesis/database/seeders/PropuestasSeeder.php
@@ -2,17 +2,20 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class PropuestasSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run()
     {
-        //
+       
+
+        DB::table('propuestas')->insert([
+            ['id' => '1', 'fecha' => Carbon::now(), 'documento' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...', 'estado' => 'aceptado', 'estudiante_rut' => '12345678-9'],
+            ['id' => 2, 'fecha' => Carbon::now(), 'documento' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.', 'estado' => 'rechazado', 'estudiante_rut' => '54321098-7'],
+            ['id' => 3, 'fecha' => Carbon::now(), 'documento' => 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur.', 'estado' => 'modificar', 'estudiante_rut' => '12345678-9'],
+        ]);
     }
 }
