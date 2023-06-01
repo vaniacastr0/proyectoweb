defd92aa3179b339ce86ae858f39e227fefda469
Vania Castro <vaniapaocastroesp@gmail.com>
2023-05-31 22:22:56 -0400

    Fix Seeders propuestas y profesor_propuesta

--- PropuestasTesis/database/seeders/ProfesoresPropuestasSeeder.php
+++ PropuestasTesis/database/seeders/ProfesoresPropuestasSeeder.php
@@ -7,17 +7,17 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-$horaActual = Carbon::now()->format('H:i:s');
 
 class ProfesoresPropuestasSeeder extends Seeder
 {
     
     public function run()
     {
-        DB::table('profesor_propuestas')->insert([
-            ['propuesta_id'=>'1','profesor_rut' => '7834358-3','fecha'=>Carbon::now(),'hora'=>$horaActual,'comentarios'=>''],
-            ['propuesta_id'=>'2','profesor_rut' => '63704068-5','fecha'=>Carbon::now(),'hora'=>$horaActual,'comentarios'=>'Hace algo nuevo tu propuesta no va a lugar'],
-            ['propuesta_id'=>'3','profesor_rut' => '6213232-9','fecha'=>Carbon::now(),'hora'=>$horaActual,'comentarios'=>'mal redactado hacelo tod de nuevo'],
+        $horaActual = Carbon::now()->format('H:i:s');
+        DB::table('profesor_propuesta')->insert([
+            ['Propuesta_Id'=>'1','Profesor_Rut' => '7834358-3','Fecha'=>Carbon::now(),'Hora'=>$horaActual,'Comentario'=>'asdasdsadsdasddsdsa'],
+            ['Propuesta_Id'=>'2','Profesor_Rut' => '63704068-5','Fecha'=>Carbon::now(),'Hora'=>$horaActual,'Comentario'=>'Hace algo nuevo tu propuesta no va a lugar'],
+            ['Propuesta_Id'=>'3','Profesor_Rut' => '6213232-9','Fecha'=>Carbon::now(),'Hora'=>$horaActual,'Comentario'=>'mal redactado hacelo tod de nuevo'],
 
         ]);    
     }
--- PropuestasTesis/database/seeders/PropuestasSeeder.php
+++ PropuestasTesis/database/seeders/PropuestasSeeder.php
@@ -10,12 +10,10 @@ class PropuestasSeeder extends Seeder
 {
     public function run()
     {
-       
-
         DB::table('propuestas')->insert([
-            ['id' => '1', 'fecha' => Carbon::now(), 'documento' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...', 'estado' => 'aceptado', 'estudiante_rut' => '12345678-9'],
-            ['id' => 2, 'fecha' => Carbon::now(), 'documento' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.', 'estado' => 'rechazado', 'estudiante_rut' => '54321098-7'],
-            ['id' => 3, 'fecha' => Carbon::now(), 'documento' => 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur.', 'estado' => 'modificar', 'estudiante_rut' => '12345678-9'],
+            ['Id' => '1', 'Fecha' => Carbon::now(), 'Documento' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...', 'Estado' => '3', 'Estudiante_Rut' => '12345678-9'],
+            ['Id' => '2', 'Fecha' => Carbon::now(), 'Documento' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.', 'Estado' => '2', 'Estudiante_Rut' => '45678901-2'],
+            ['Id' => '3', 'Fecha' => Carbon::now(), 'Documento' => 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur.', 'Estado' => '1', 'Estudiante_Rut' => '78901234-5'],
         ]);
     }
 }
